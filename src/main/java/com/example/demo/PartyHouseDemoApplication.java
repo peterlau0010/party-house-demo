0629fe2e2db96aa73d938d380699b36cb8fa2e10
Peter Lau <peterl@hk1.ibm.com>
2018-12-03 23:38:09 +0800

    Add deployment tomcat config

--- src/main/java/com/example/demo/PartyHouseDemoApplication.java
+++ src/main/java/com/example/demo/PartyHouseDemoApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
-public class PartyHouseDemoApplication {
+public class PartyHouseDemoApplication extends SpringBootServletInitializer{
 	
 //	private static final Logger log = LogManager.getLogger(PartyHouseDemoApplication.class);
 	public static void main(String[] args) {
